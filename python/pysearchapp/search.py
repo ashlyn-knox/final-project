3eb1a660c10fa812ef6abb7bc1a8fedc37472087
ashlyn-knox <tristan.knox@edu.sait.ca>
2021-04-22 09:43:46 -0600

    search app correctly analyzes json data and uses it to create a series of google searches

--- python/pysearchapp/search.py
+++ python/pysearchapp/search.py
@@ -8,13 +8,20 @@ from settings import mongo
 from pprint import pprint
 
 # # connect to mongodb
-client = MongoClient(mongo)
-db=client.admin
+# client = MongoClient(mongo)
+# db=client.admin
 # # print results of server status
-serverStatusResults=db.command("serverStatus")
+# serverStatusResults=db.command("serverStatus")
 # pprint(serverStatusResult)
 
 # Read JSON file for search terms TODO move this to a separate file and then import it for assigning values to queries etc
+
+# Connect this to database or to api for reading
+form_data = open("../test.json", "r")
+
+form_results = json.load(form_data)
+
+form_item = list(form_results)
 
 # ## SERVER VARIABLES ### #
 framework = {
@@ -41,69 +48,67 @@ databases = {
     "sql": 0
 }
 # Test input
-python = input('Python yes or no: ')
-javascript = input('Javascript yes or no: ')
-print(javascript)
-ruby = input('ruby yes or no: ')
-print(ruby)
-php = input('php yes or no: ')
-print(php)
+python = form_item[0]
+print(python)
+javascript = form_item[1] 
+ruby = form_item[2]
+php = form_item[3]
 
 # set to boolean data type for production
 if python == True: 
     framework['django'] += 5
     framework['flask'] += 5
     frontend['jinja'] += 1
-elif javascript == 'yes':
+elif javascript == True:
     framework['express'] += 5
     frontend['ejs'] += 1
     frontend['react'] += 2
     frontend['vue'] += 2
     frontend['angular'] += 2
-elif ruby == 'yes':
+elif ruby == True:
     framework['rubyonrails'] += 5
     framework['sinatra'] += 5
-elif php == 'yes':
+elif php == True:
     framework['laravel'] += 5
     frontend['blade'] += 1
 else:
-    print('ERROR')
+    print('Error: Items not assigning values')
 
 # if the user wants to try a new language
 # TODO logic: IF any in the language equaption is false, add +3 to it. this should be in the same function
 
 # TODO write code that analyzes the form results here and then put that in a separate file that sends data to here
     # calculate scores and return all values
-mature = input('mature framework yes or no: ')
-scalable = input('scalable importance yes no')
-fastDev = input('fastDev yes no')
-realTime = input('Real time function yes no')
-machineAi = input('Machinelearning y n')
-microframe = input('Do you want a microframework')
-if mature == 'yes':
+mature = form_item[4]
+scalable = form_item[5] 
+fastDev = form_item[6]
+realTime = form_item[7] 
+machineAi = form_item[8]
+microframe = form_item[9]
+if mature == True:
     framework['django'] += 2
     framework['rubyonrails'] += 2
-if scalable == 'yes':
+if scalable == True:
     framework['django'] += 2
     framework['express'] += 2
-if fastDev == 'yes':
+if fastDev == True:
     framework['flask'] += 2
     framework['express'] += 2
     framework['sinatra'] += 2
-if realTime == 'yes':
+if realTime == True:
     framework['express'] += 2
-if machineAi == 'yes':
+if machineAi == True:
     framework['django'] += 2
     framework['flask'] += 2
-if microframe == 'yes':
+if microframe == True:
     framework['express'] += 2
     framework['flask'] += 2
     framework['laravel'] += 2
     framework['sinatra'] +=2
 
     # calculate scores and return all values
-flexibleData = input('Flexible data model?')
-structuredData = input('structured data?')
+flexibleData = form_item[9]
+structuredData = form_item[10] 
 if flexibleData == 'yes':
         databases['nosql'] += 5
 if structuredData == 'yes':
